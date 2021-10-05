5ba2c166a127c0e97feecdc0fbbd90cba58fbe56
AJOsmaston <dajoxx@gmail.com>
2021-10-05 15:27:30 +0100

    added named-cat with input form

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do 
-    "Hello World"
-end 
+# get '/' do 
+#   "Hello World"
+# end 
 
-get '/secret' do
-    "There is a secret garden"
+# get '/secret' do
+#   "There is a secret garden"
+# end
+
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
 end
 
-get '/cat' do
-    @names = ["Amigo", "Misty", "Almond"].sample
-    erb(:index)
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
-
